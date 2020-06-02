4cc12dbd64b181df7c51e8f5202a8fb0d9fb7e50
unknown <quino.terrasa+dev@gmail.com>
2020-06-02 19:43:14 +0200

    Added cursive style removing (but doesnt work)

--- textRecognition/__init__.py
+++ textRecognition/__init__.py
@@ -1,4 +1,4 @@
 # Functions to obtain text from word images
 from textRecognition.model import WordHTRFlor, FullGatedConv2D
 from textRecognition.tinydata import TinyData
-from textRecognition.dataunpack import DataUnpack
+from textRecognition.dataunpack import DataUnpack, StringVectorizer
--- textRecognition/dataunpack.py
+++ textRecognition/dataunpack.py
@@ -1,3 +1,6 @@
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from preproc import RobustBinarize
+from unicodedata import normalize
 from numpy import ndarray
 
 import pandas as pd
@@ -65,10 +68,12 @@ class DataUnpack:
         
 
     @staticmethod
-    def unpack_set(set_name, dest_dir, file, color, target_size=None, aspect_ratio=None):
+    def unpack_set(set_name, dest_dir, file, color, target_size=None, aspect_ratio=None, max_word_length=34):
         """Performs the unpacking step for each dataset split (train/test/validation)"""
         result = pd.DataFrame(columns=["name", "label"])
-        names, labels = [], []
+        vectorizer = StringVectorizer(max_word_length=34)
+
+        names, labels, vector_labels = [], [], [f"X{i}" for i in range(max_word_length)]
 
         os.makedirs(f"{dest_dir}/{set_name}", exist_ok=True)
 
@@ -83,25 +88,37 @@ class DataUnpack:
             if target_size and aspect_ratio: # are not None
                 image = DataUnpack.resize(sample_name, image, target_size, aspect_ratio)
 
+            # Remove cursive style (WORKS BADLY FOR 'train_spa/w02_bw_22' and takes so much time)
+            # image = RobustBinarize.remove_cursive_style(image)
+
             # at the time of writing, it should follow the folder structure for RIMES and IAM
             DataUnpack.imwrite(f"{dest_dir}/{set_name}/{sample_name}.png", image, color=color)
 
         result["name"] = names
+        result["fname"] = result["name"].apply(lambda x: x + ".png")  # add the filename
         result["label"] = labels
+        
+        # create a column for each encoded character label
+        for lb in vector_labels:
+            result[lb] = int()
+        # fill all encoded character labels
+        for idx, row in result[["label"]].iterrows():
+            result.loc[idx, vector_labels] = vectorizer.vectorize(row.label)
+
         return result
 
     @staticmethod
-    def unpack(input_path, dest_dir, color=False, save_df=False, target_size=None, aspect_ratio:float=None):
+    def unpack(input_path, dest_dir, color=False, save_df=False, target_size=None, aspect_ratio:float=None, max_word_length=34):
         """Deminifies / Unpacks the compressed file into train/test/validation DataFrames.
         Images are saved in .png format.
         """
         with h5.File(input_path, "r") as f:
-            train = DataUnpack.unpack_set("train", dest_dir, f, color, target_size, aspect_ratio)
-            test = DataUnpack.unpack_set("test", dest_dir, f, color, target_size, aspect_ratio)
+            train = DataUnpack.unpack_set("train", dest_dir, f, color, target_size, aspect_ratio, max_word_length)
+            test = DataUnpack.unpack_set("test", dest_dir, f, color, target_size, aspect_ratio, max_word_length)
 
             # if there is a valid split stored, then extract it as well
             if f.get("valid", None):
-                valid = DataUnpack.unpack_set("valid", dest_dir, f, color, target_size, aspect_ratio)
+                valid = DataUnpack.unpack_set("valid", dest_dir, f, color, target_size, aspect_ratio, max_word_length)
             else:
                 valid = None
 
@@ -111,3 +128,51 @@ class DataUnpack:
             valid.to_csv(f"{dest_dir}/valid/valid.csv", sep=",", index=False)
 
         return train, test, valid
+
+
+
+class StringVectorizer:
+    """A set of functions to encode strings into integer arrays given a character set.
+    
+    Parameters
+    ----------
+        max_word_length: int, Default 34
+            Set by the most common longest word in French, English and Spanish
+            Sources:
+                https://en.wikipedia.org/wiki/Longest_word_in_Spanish
+                https://en.wikipedia.org/wiki/Longest_word_in_English
+                https://en.wikipedia.org/wiki/Longest_word_in_French
+    """
+    # CHARSET = WordHTRFlor.LATIN_CHAR
+    CHARSET = " !\"#$%&'()*+,-.0123456789:;<>@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzáÁéÉíÍóÓúÚëËïÏüÜñÑçÇâÂêÊîÎôÔûÛàÀèÈùÙ"
+    
+    def __init__(self, max_word_length=34):
+        self.PAD_TK, self.UNK_TK = "¶", "¤"
+        self.chars = (self.PAD_TK + self.UNK_TK + StringVectorizer.CHARSET)
+
+        self.PAD = self.chars.find(self.PAD_TK)
+        self.UNK = self.chars.find(self.UNK_TK)
+
+        self.vocab_size = len(self.chars)
+        self.maxlen = max_word_length
+    
+    def vectorize(self, word, as_onehot=False):
+        """Encode word to integer vector (or binary vector if 'as_onehot' set to True)
+        Spanish characters work with NFKC, not NFKD (https://docs.python.org/3/library/unicodedata.html)
+
+        Source:
+            https://github.com/arthurflor23/handwritten-text-recognition/blob/master/src/data/generator.py#L115
+        """
+        word = normalize("NFKC", word).encode("UTF-8", "ignore").decode("UTF-8")
+        
+        vector = (self.chars.find(word[i]) for i in range(len(word)))
+        vector = [num if num != -1 else self.UNK for num in vector]
+                
+        padded_vector = pad_sequences([vector], maxlen=self.maxlen, padding="post")
+        return padded_vector[0]
+        
+    def decode(self, vector, as_onehot=False):
+        """Inverses the vectorization"""
+        chars = "".join([self.chars[pos] for pos in vector])
+        return chars.replace(self.PAD_TK, "").replace(self.UNK_TK, "")
+    
