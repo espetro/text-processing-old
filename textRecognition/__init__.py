f575bd9fe75ac67758ed5ad8ef73e445600592bc
unknown <quino.terrasa+dev@gmail.com>
2020-06-04 18:41:00 +0200

    Hotfix 2 on StringVectorizer (as the one before :( )

--- textRecognition/__init__.py
+++ textRecognition/__init__.py
@@ -1,5 +1,5 @@
 # Functions to obtain text from word images
 from textRecognition.model import WordHTRFlor, FullGatedConv2D
+from textRecognition.vectorizer import StringVectorizer
 from textRecognition.dataunpack import DataUnpack
 from textRecognition.tinydata import TinyData
-from textRecognition.vectorizer import StringVectorizer
