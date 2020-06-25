0bb81ddd31eb34ca5315b209fe0f19a882091485
unknown <quino.terrasa+dev@gmail.com>
2020-06-26 00:19:45 +0200

    Shrinked highlight recognition network.

--- recognition/color.py
+++ recognition/color.py
@@ -1,6 +1,7 @@
 from preproc import Quantize
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, BatchNormalization, MaxPooling2D, Flatten
+from keras.layers import SeparableConv2D
 from keras.wrappers.scikit_learn import KerasClassifier
 
 from sklearn.model_selection import cross_val_score
@@ -29,18 +30,18 @@ class HighlightDetector:
     
     NUM_CLASSES = 2
     TARGET_SIZE = (150,150, 3)
-    MODEL_PATH = pkg_resources.files("recognition.data").joinpath("highlight_model_30e.h5")
+    MODEL_PATH = pkg_resources.files("recognition.data").joinpath("highlight_model_mini_45e_64bz_weights.ckpt")
     # remember to add __init__.py to data/ folder
 
-    def __init__(self, target_size=None):
+    def __init__(self, target_size=None, epochs=1):
         self.input_size = target_size or HighlightDetector.TARGET_SIZE
 
-        self.model = KerasClassifier(
+        self.net = KerasClassifier(
             build_fn=HighlightDetector._build_model,
             input_size=self.input_size,
-            epochs=30,
-            batch_size=128,
-            verbose=1
+            epochs=1,
+            batch_size=32,
+            verbose=0
         )
 
     @staticmethod    
@@ -70,18 +71,25 @@ class HighlightDetector:
     @staticmethod
     def _build_model(input_size):
         model = Sequential()
-
-        model.add(Conv2D(32, (3,3), activation="relu", input_shape=input_size, name="conv1"))
+    
+        model.add(SeparableConv2D(32, (3,3), activation="relu", input_shape=(150, 150, 3), name="conv1"))
         model.add(MaxPooling2D((2,2), name="pool1"))
         
-        model.add(Conv2D(64, (3,3), activation="relu", name="conv2"))
+        model.add(SeparableConv2D(16, (3,3), activation="relu", name="conv2"))
         model.add(MaxPooling2D((2,2), name="pool2"))
+
+        model.add(Conv2D(8, (3,3), activation="relu", name="conv3"))
+        model.add(MaxPooling2D((2,2), name="pool3"))
+
+        model.add(Conv2D(4, (3,3), activation="relu", name="conv4"))
+        model.add(MaxPooling2D((2,2), name="pool4"))
         
         model.add(Flatten())
-        model.add(Dense(64, activation='relu', name="dense1"))
+        
+        model.add(Dense(8, activation='relu', name="dense1"))
         model.add(BatchNormalization())
         model.add(Dense(1, activation='sigmoid', name="dense2"))
-        
+
         # Compile model
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
         return model
@@ -100,22 +108,28 @@ class HighlightDetector:
         plt.show()
 
     def train(self, X_train, Y_train, epochs=30, plot=False):
-        training_results = self.model.fit(X_train, Y_train, verbose=1)
+        training_results = self.net.fit(X_train, Y_train, verbose=1)
         if plot:
             HighlightDetector.plot_results(training_results)
 
     def predict(self, X_test):
         """"""
-        return self.model.predict_classes(X_test).flatten()
+        return self.net.model.predict_classes(X_test).flatten()
 
     def cross_validate(self, X, Y, k=10, epochs=30, batch_sz=128):
         kfold = StratifiedKFold(n_splits=k, shuffle=True)
-        results = cross_val_score(self.model, X, Y, cv=kfold)
+        results = cross_val_score(self.net, X, Y, cv=kfold)
+
         print(f"Baseline: {(results.mean() * 100):.2f}% ({(results.std()*100):.2f}%)")
 
-    def load_model(self, fpath=None):
+    def load_weights(self, fpath=None):
         fpath = fpath or str(HighlightDetector.MODEL_PATH)
-        self.model = keras.models.load_model(fpath)
+
+        # initialize the network
+        dummyX, dummyY = np.zeros((1,150,150,3)), np.zeros((1))
+        _ = self.net.fit(dummyX, dummyY, verbose=0)
+
+        self.net.model.load_weights(fpath)
 
 
 # =====================================
